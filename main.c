07759e2523aabfdbf76680e314be196e48e11cef
h-s-samra <hssamra2010@gmail.com>
2017-02-06 18:10:06 -0800

    added build number to main.c.  added common.h to hold header files and global variables.

--- main.c
+++ main.c
@@ -5,10 +5,40 @@
 // -------------------------------------------------
 
 #include <stdio.h>
+#include <string.h>
+#include "common.h"
+
+const char filename[] = {"bld_num.txt"};
+byte vrsn_mjr = 0x01;
+byte vrsn_mnr = 0x00;
+char vrsn_rls = 'A' - 0;
+char vrsn_bld[8];
+
+byte _read_buildNum(char *str)
+{
+	FILE *file = fopen(filename, "r");
+	
+	memset(vrsn_bld, 0x00, sizeof(vrsn_bld));
+	
+	if(!fgets(str, sizeof(str), file))
+	{
+		printf("\r\nError: Failed to read build number");
+		return 0;
+	}
+	
+	fclose(file);
+	
+	printf("\r\nRoman Numeral Calculator\r\n");
+	printf("Version: v%d.%d%c.%s\r\n\n", vrsn_mjr, vrsn_mnr, vrsn_rls, vrsn_bld);
+}
 
 int main(void)
 {
-	printf("Hello World!");
+	if(!_read_buildNum(vrsn_bld))
+	{
+		return 0;
+	}
+	
 	
 	return 0;
 }
